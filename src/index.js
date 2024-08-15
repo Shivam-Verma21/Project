8fd3a9b11b10fb80896f20cdc6ec03fb657f801e
Shivam Verma <shivamverma21201@gmail.com>
2024-08-15 18:36:09 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 // import express from "express";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error(error);
+      throw error;
+    });
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(
+        `Server is running on port: http://localhost:${process.env.PORT}/`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection failed!! ", err);
+  });
 
 /*
 const app = express();
@@ -32,5 +50,3 @@ const app = express();
     }
 })();
 */
-
-
